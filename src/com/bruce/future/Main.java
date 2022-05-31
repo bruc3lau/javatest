1192443c396c081a6dcdd3d65293ce9f969f6449
QingXin Liu <qxliu@aibee.com>
2022-05-31 18:44:31 +0800

    init

---
+++ src/com/bruce/future/Main.java
@@ -0,0 +1,12 @@
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+class Main {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        FutureTask<String> ft = new FutureTask<String>(() -> {
+            return "good";
+        });
+        ft.run();
+        System.out.println(ft.get());
+    }
+}
