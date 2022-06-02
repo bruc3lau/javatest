2502a7ba48c1fa49f8db8b0b309fd89fe36ca910
QingXin Liu <qxliu@aibee.com>
2022-06-02 19:30:05 +0800

    update spinlock

--- src/main/java/com/bruce/atomic/SpinLock.java
+++ src/main/java/com/bruce/atomic/SpinLock.java
@@ -7,30 +7,28 @@ public class SpinLock {
     AtomicReference<Thread> r = new AtomicReference<Thread>();
 
     public void lock() {
+        System.out.println(Thread.currentThread().getName() + "==come in");
         while (!r.compareAndSet(null, Thread.currentThread())) {
-            System.out.println("locking===");
+            System.out.println(Thread.currentThread().getName() + "===swap false, will retry lock");
         }
     }
 
     public void unlock() {
-        while (!r.compareAndSet(Thread.currentThread(), null)) {
-            System.out.println("unlocking===");
-        }
+        r.compareAndSet(Thread.currentThread(), null);
+        System.out.println(Thread.currentThread().getName() + "==unlock");
     }
 
     public static void main(String[] args) {
         SpinLock spinLock = new SpinLock();
         new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + "lock");
             spinLock.lock();
             try {
-                TimeUnit.SECONDS.sleep(3);
+                TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
             spinLock.unlock();
-            System.out.println(Thread.currentThread().getName() + "unlock");
         }, "t1").start();
         try {
             TimeUnit.MILLISECONDS.sleep(100);
@@ -39,10 +37,8 @@ public class SpinLock {
             e.printStackTrace();
         }
         new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + "lock");
             spinLock.lock();
             spinLock.unlock();
-            System.out.println(Thread.currentThread().getName() + "unlock");
         }, "t2").start();
     }
 }
